e549da6fddb9531ac549a0b22c1506f55939f24c
r1d20 <woody.garcia42@gmail.com>
2023-10-17 23:24:49 -0400

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mother-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.sass']
+})
+export class AppComponent {
+  title = 'mother-app';
+}
